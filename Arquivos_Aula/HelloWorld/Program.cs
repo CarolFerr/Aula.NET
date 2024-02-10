6e4adac6aa695c849e1d38a47418b4785999b4ea
CarolFerr <114115953+CarolFerr@users.noreply.github.com>
2024-02-10 13:46:35 -0300

    Add #1 class .NET
    Introdução a linguagem .NET e CSharp para aprendizado de Desenvolvimento de Sistemas de Informação.

---
+++ Arquivos_Aula/HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace HelloWorld; //nome do projeto o equivalente em java a package
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numeroAnterior = 0;
+        int numeroAtual = 1;
+        int fibonacci;
+
+        for (int i = 0; i < 10; i++)
+        {
+            fibonacci = numeroAnterior + numeroAtual; 
+            Console.WriteLine(fibonacci);
+            numeroAnterior = numeroAtual;
+            numeroAtual = fibonacci;
+        }
+        
+        
+    }
+}
